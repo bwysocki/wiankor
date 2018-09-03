cb9bf1b1d9e75a828afba040cf6dfdbe91034065
Bartosz Wysocki <wysockibartosz@gmail.com>
2018-09-03 21:39:26 +0200

    Silnik

--- silnik/go.py
+++ silnik/go.py
@@ -5,14 +5,18 @@ import os
 sqs = boto3.client('sqs', region_name='eu-central-1', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 queue_url = 'https://sqs.eu-central-1.amazonaws.com/719069272797/wiankor-silnik'
 
-# Long poll for message on provided SQS queue
-response = sqs.receive_message(
-    QueueUrl=queue_url,
-    MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
-    WaitTimeSeconds=20
-)
+try:
+  while True:
+    # Long poll for message on provided SQS queue
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        WaitTimeSeconds=20
+    )
 
-print(response['Messages'][0]['Body'])
+    print(response['Messages'][0]['Body'])
+finally:
+  print("Czyszczenie")
