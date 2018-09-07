f3a22f0167ba4cb15be197bcfd28b8cbf5d3836f
Bartosz Wysocki <wysockibartosz@gmail.com>
2018-09-07 21:47:24 +0200

    silnik

--- silnik/go.py
+++ silnik/go.py
@@ -1,13 +1,46 @@
 import boto3
 import os
 import sys
+import RPi.GPIO as GPIO
+import time
 
 # Create SQS client
 sqs = boto3.client('sqs', region_name='eu-central-1', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 queue_url = 'https://sqs.eu-central-1.amazonaws.com/719069272797/wiankor-silnik'
 
-try:
-  while True:
+GPIO.setmode(GPIO.BCM)
+
+enable_pin = 18
+in_1_pin = 23
+in_2_pin = 24
+
+GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(in_1_pin, GPIO.OUT)
+GPIO.setup(in_2_pin, GPIO.OUT)
+motor_pwm = GPIO.PWM(enable_pin, 500)
+motor_pwm.start(0)
+
+def forward(duty):
+    GPIO.output(in_1_pin, True)
+    GPIO.output(in_2_pin, False)
+    motor_pwm.ChangeDutyCycle(duty)
+
+def reverse(duty):
+    GPIO.output(in_1_pin, False)
+    GPIO.output(in_2_pin, True)
+    motor_pwm.ChangeDutyCycle(duty)
+
+def stop():
+    GPIO.output(in_1_pin, False)
+    GPIO.output(in_2_pin, False)
+    motor_pwm.ChangeDutyCycle(0)
+
+def cycle():
+    forward(30)
+    time.sleep(1)
+    stop()
+
+while True:
     # Long poll for message on provided SQS queue
     response = sqs.receive_message(
         QueueUrl=queue_url,
@@ -26,6 +59,7 @@ try:
             QueueUrl=queue_url,
             ReceiptHandle=receipt_handle
         )
+        cycle()
         nrOfCycles = message['Body'].split("=")[1].strip()
         print(nrOfCycles)
     else:
